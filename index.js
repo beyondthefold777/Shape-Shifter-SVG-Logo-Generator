aa6068631d13209edc0cc8ec94558f6f1d0e7642
anthony bell <anthonybell67.ab@gmail.com>
2024-05-15 18:57:22 -0400

    updated readme

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const {Circle, Square, Triangle} = require('./shapes')
+
+// Array of questions for user input
+const questions = [
+    {  
+      type: 'input',
+      name: 'title',
+      message: 'What is your user name?',
+    },
+    {  
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+    },
+    {  
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email',
+    },
+    {  
+      type: 'input',
+      name: 'description',
+      message: 'What is the purpose and functionality of this project?',
+    },
+]
+
